234b1394bf3c0749ff01be8753bd3827501c60f1
khalilcodes <khalilali.j@gmail.com>
2022-11-25 03:48:58 +0400

    [tests][s]: update tests

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,7 +1,22 @@
 const fs = require("fs");
 const path = require("path");
-const parser = require('../src/parser.js');
+const parser = require("../src/parser.js");
 const writer = require( "../src/writer.js");
+
+let files = [];
+
+const getFilesRecursively = (directory) => {
+  const filesInDirectory = fs.readdirSync(directory);
+  for (const file of filesInDirectory) {
+    const absolute = path.join(directory, file);
+    if (fs.statSync(absolute).isDirectory()) {
+        getFilesRecursively(absolute);
+    } else {
+        files.push(absolute);
+    }
+  }
+  return files
+};
 
 const destDir = 'tests/output'
 
@@ -14,41 +29,41 @@ const defaultConfig = {
   prefixDate: false,
   saveAttachedImages: false,
   saveScrapedImages: false,
-  includeOtherTypes: false
+  includeOtherTypes: true
 }
 
-test("creates an output directory with markdown files", async () => {
-  const posts = await parser.parseFilePromise(defaultConfig)
-  const slicedPosts = posts.slice(0,2)
+const testPost = "tests/output/post/non-attachment-to-views-by-jonathan-ekstrom.md"
 
-  fs.rmSync(destDir, { recursive: true, force: true });
-  await writer.writeFilesPromise(slicedPosts, defaultConfig);
-
-  expect(fs.existsSync(destDir)).toBe(true)
-
-})
-
-const content = `---
-title: Nine Theses
-date: 2017-05-23
+const testFrontmatter =
+`---
+title: "Jonathan Ekstrom: Non Attachment to Views"
+date: 2016-10-06
 categories: 
-  - our-philosophy
+  - book-notes
 tags: 
-  - rufus-pollock
-  - theses
+  - jonathan-ekstrom
+  - non-attachment
 ---`
 
-test("outputs the frontmatter in markdown", async () => {
-  const posts = await parser.parseFilePromise(defaultConfig)
-  const slicedPosts = posts.slice(0,2)
+test("creates an output directory with markdown files", async () => {
+  const allDocs = await parser.parseFilePromise(defaultConfig)
+
+  const pages = allDocs.filter(p => ["page"].includes(p.meta.type)).slice(0,2)
+  const posts = allDocs.filter(p => ["post"].includes(p.meta.type)).slice(0,2)
+
+  const slicedPosts = [ ...pages, ...posts ]
 
   fs.rmSync(destDir, { recursive: true, force: true });
   await writer.writeFilesPromise(slicedPosts, defaultConfig);
 
-  const file = fs.readdirSync(destDir)[0]
-  const fileContent = fs.readFileSync(path.join(destDir + "/" + file), { encoding: "utf8"})
-
   expect(fs.existsSync(destDir)).toBe(true)
-  expect(fileContent).toContain(content)
+})
 
+test("outputs the frontmatter in markdown", () => {
+  getFilesRecursively(destDir)
+  const file = files.find(f => f === testPost)
+  const fileContent = fs.readFileSync(file, "utf8")
+  const fileFrontmatter = fileContent.substring(0, fileContent.lastIndexOf("---") + 3)
+
+  expect(fileFrontmatter).toMatch(testFrontmatter)
 })
